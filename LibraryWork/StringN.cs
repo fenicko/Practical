dee579ecc48ab74ece9f2b62fca5c440ddb42081
Mikhail <fenikohai@mail.ru>
2023-03-30 14:55:07 +0300

    AddCheckErrors

--- LibraryWork/StringN.cs
+++ LibraryWork/StringN.cs
@@ -1,29 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LibraryWork
 {
     public class StringN
     {
-        private string errorString = "Error: Введено неправильное значение!";
-        private string errorNumber = "Error: Введите число больше 0!";
-
         public string GetIntLine(string getN)
         {
             try
             {
-                int n = Convert.ToInt32(getN);
-
-                if (n <= 0)
+                if (int.TryParse(getN, out int x) == false)
                 {
-                    return errorNumber;
+                    throw new ArgumentNullException(null, "Error: Введено неправильное значение. Введите число!");
+                }
+                else
+                {
+                    if (x <= 0)
+                    {
+                        throw new ArgumentNullException(null, "Error: Введите число больше нуля!");
+                    }
                 }
 
+                int n = Convert.ToInt32(getN);
+
                 string returnLine = $"N={n}: ";
+
                 for (int i = 1; i <= n; i++)
                 {
                     if (i < n)
@@ -35,26 +35,32 @@ namespace LibraryWork
                         returnLine += $"{i}.";
                     }
                 }
+
                 return returnLine;
-
             }
-            catch
+            catch (ArgumentNullException error)
             {
-                return errorString;
+                return error.Message;
             }
-
         }
 
         public string GetIntLattices(string getN)
         {
             try
             {
-                int n = Convert.ToInt32(getN);
-
-                if (n <= 0)
+                if (int.TryParse(getN, out int x) == false)
                 {
-                    return errorNumber;
+                    throw new ArgumentNullException(null, "Error: Введено неправильное значение. Введите число!");
                 }
+                else
+                {
+                    if (x <= 0)
+                    {
+                        throw new ArgumentNullException(null, "Error: Введите число больше нуля!");
+                    }
+                }
+
+                int n = Convert.ToInt32(getN);
 
                 string returnLine = "";
                 string[,] array = new string[n, n];
@@ -63,8 +69,8 @@ namespace LibraryWork
                 {
                     for (int j = 0; j < array.GetLength(1); j++)
                     {
-                        //array[0, 0]          array[0, n]                array[n, 0]               array[n, n]                    array[center]
-                        array[i, j] = ((i == 0 && j == 0) || (i == 0 && j == n - 1) || (i == n - 1 && j == 0) || (i == n - 1 && j == n - 1) || (i == (n - 1) / 2 && j == (n - 1) / 2)) ? " " : "#";
+                                          //array[0, 0]          array[0, n]                   array[n, 0]               array[n, n]                        array[center]
+                        array[i, j] = (i == 0 && j == 0) || (i == 0 && j == n - 1) || (i == n - 1 && j == 0) || (i == n - 1 && j == n - 1) || (i == (n - 1) / 2 && j == (n - 1) / 2) ? " " : "#";
                         returnLine += array[i, j];
                     }
                     returnLine += "\n";
@@ -72,9 +78,9 @@ namespace LibraryWork
 
                 return returnLine;
             }
-            catch
+            catch (ArgumentNullException error)
             {
-                return errorString;
+                return error.Message;
             }
         }
 
